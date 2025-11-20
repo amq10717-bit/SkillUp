785d024bbf6e770eee8d863beee983932f4da9e0
aliqureshi12 <amq10717@gmail.com>
2025-11-21 00:02:24 +0500

    fix error 1 before vercel

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './App.css';
-import './styles/global.css';
+import './styles/Global.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
